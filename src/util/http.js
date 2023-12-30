606e6f2ae5529fa525f06820774118344a90ce70
ZeyadFarhat11 <zeyadfarhat145@gmail.com>
2023-12-30 13:26:04 +0200

    add http url to env

--- src/util/http.js
+++ src/util/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { userTokenKey, userKey } from "./auth";
 
-const http = axios.create({ baseURL: "http://127.0.0.1:8000/api" });
+const http = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 window.http = http;
 
 http.interceptors.request.use(
